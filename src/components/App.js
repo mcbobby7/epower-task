215cd6ef138f1e7e5227f860cd23f383e6db1002
mcbobby <madumcbobby@yahoo.com>
2019-10-09 20:22:39 +0100

    feat: hitting the API and returning data from the Wordpress API

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Posts from './posts';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [posts, setPosts] = useState({});
+    const [loading, setLoading] = useState({});
+    const [currentPage, setCurrentPage] = useState({});
+    const [postsPerPage, setPostsPerPage] = useState({});
+
+    useEffect(() => {
+        const fetchPost = async () => {
+            setLoading(true);
+            const res = await axios.get('https://epower.ng/wp-json/wp/v2/posts');
+            setPosts(res.data);
+            setLoading(false);
+        };
+
+        fetchPost();
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>My Blog</h1>
+            <Posts posts={posts} loading={loading} />
+        </div>
+    );
+};
 
 export default App;
